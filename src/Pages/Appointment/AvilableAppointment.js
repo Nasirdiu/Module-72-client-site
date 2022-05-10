58e8097149f7a9cfa5ed46d21d6a8db828cdb73d
nasirdiu <nasir.joypurhat@gmail.com>
2022-05-10 22:37:37 +0600

    service doctor

--- src/Pages/Appointment/AvilableAppointment.js
+++ src/Pages/Appointment/AvilableAppointment.js
@@ -1,12 +1,30 @@
-import { format } from 'date-fns';
-import React from 'react';
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import AppointmentServices from "./AppointmentServices";
 
-const AvilableAppointment = ({date}) => {
-    return (
-        <div>
-           <h4 className='text-secondary font-bold text-center text-xl'> Avilable Appointment on {format(date, "PP")}</h4>
-        </div>
-    );
+const AvilableAppointment = ({ date }) => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch(`services.json`)
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <h4 className="text-secondary font-bold text-center text-xl">
+        {" "}
+        Avilable Appointment on {format(date, "PP")}
+      </h4>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {services.map((service) => (
+          <AppointmentServices
+            key={service._id}
+            service={service}
+          ></AppointmentServices>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AvilableAppointment;
